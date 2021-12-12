b2f5bada18d18e47619a55b09177da25336da39b
Kappaguard <a.chatulov@gmail.com>
2021-12-12 21:52:28 +0300

    improved algorithm

--- euclid_algorithm.py
+++ euclid_algorithm.py
@@ -1,36 +1,34 @@
-import math 
+a_custom=25 # int(input('1 число: '))
+b_custom=5 # int(input('2 число: '))
 
-a=23 # int(input('1 число: '))
-b=32 # int(input('2 число: '))
-
-def euclid(a1,b2):
+def euclid(a_1,b_2):
     """Осуществляет алгоритм Евклида для целых чисел"""
-    dels=[]
-    if a==0:
-        return a1
-    elif b==0:
-        return b2   
-    
-    y= min (a1,b2)
-    x= max(a1,b2)
-    r= 0
-    p=0
 
-    while (x > 0):
-        p=r
-        r = x % y
-        if r==0:
-           # print(x,p,r)
+    if a_1==0:
+        return b_2
+    elif b_2==0:
+        return a_1
+
+    y= min (a_1,b_2)
+    x= max(a_1,b_2)
+    r_ost= 0
+    p_ost=0
+
+    while x > 0:
+        p_ost=r_ost
+        r_ost = x % y
+        if r_ost==0:
             break
         x = (x // y)*y
-        y= r
-    if p==0:
-        return min(a1,b2)
-    elif (a1 % p !=0) or (b2 % p !=0):
-        p=min(euclid(a1,p), euclid(b2,p))
-        return(p)
+        y= r_ost
+    if p_ost==0:
+        return min(a_1,b_2)
+    elif (a_1 % p_ost !=0) or (b_2 % p_ost !=0):
+        p_ost=min(euclid(a_1,p_ost), euclid(b_2,p_ost))
+        return(p_ost)
     else:
-        return(p)
+        return(p_ost)
 
 
-print(euclid(a,b))
+print(euclid(a_custom,b_custom))
+

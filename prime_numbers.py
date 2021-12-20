67fc63b1258f991abaa906f71eb9ef8a9d1e866e
KappaGuard <a.chatulov@gmail.com>
2021-12-20 23:27:06 +0300

    <2 fix

--- prime_numbers.py
+++ prime_numbers.py
@@ -2,7 +2,7 @@ import math
 
 
 def is_prime(number):
-    if number == 0 or number == 1:
+    if number < 2:
         return False
     for i in range(2, int(math.sqrt(number)) + 1):
         if (number % i == 0) and (i != 1):
